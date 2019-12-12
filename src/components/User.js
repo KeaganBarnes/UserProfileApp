f6bde925ee4ca5d37cd910c3c24581c71ba378cb
Keagan Barnes <barnes.keagan9@gmail.com>
2019-12-12 15:28:43 -0700

    assignment completed

---
+++ src/components/User.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { Card, } from 'semantic-ui-react';
+import { UserConsumer } from '../providers/UserProvider';
+
+const User = () => {
+  const { username, dateJoined, membershipLevel} = useContext(UserContext);
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header> {username} </Card.Header>
+        <Card.Meta>
+          Date Joined: {dateJoined}
+        </Card.Meta>
+      </Card.Content>
+      <Card.Content>
+        <p> Membership Level: {membershipLevel} </p>
+      </Card.Content>
+    </Card>
+  )
+}
+export default User;
